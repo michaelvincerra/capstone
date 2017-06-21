7f29be204dee7806e9fc302991ced21bc5481df7
michaelvincerra <michael.vincerra@gmail.com>
2017-06-21 16:02:48 -0700

    Accounts app: adds forms,templates, urls, and static

---
+++ accounts/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import User
+
+
+class CustomUserCreationForm(UserCreationForm):
+    nickname = forms.RegexField(label=("Nickname"), max_length=250,
+                                regex=r'^[\w+\-\s]+$')  # +$ is for more than one character; customize as needed.
+
+
+    class Meta(UserCreationForm):
+        model = User
+        fields = UserCreationForm.Meta.fields + ('nickname',)
+
+#
+# class CustomeUserChangeForm(UserChangeForm):
+#     class Meta(UserChangeForm):
+#         model = User
+#         fields = UserChangeForm.Meta.fields + ('nickname',)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    """
+    All users. Hungry data sandwich eaters.
+    """
+    nickname = models.CharField(max_length=250)
+    # date_of_birth = models.DateField()
+    REQUIRED_FIELDS = ['nickname', 'email']
+
+
+
+
+
+# https://docs.djangoproject.com/en/1.11/topics/auth/customizing/#django.contrib.auth.models.CustomUser.REQUIRED_FIELDS
